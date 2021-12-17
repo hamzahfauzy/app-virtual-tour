bcf702f811071bca069e0e9d3f84552f4d48bcd0
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-12-17 15:30:51 +0700

    update gitignore

---
+++ config/icon_menu.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dashboard' => 'fas fa-home',
+    'master' => 'fas fa-layer-group',
+    'pengguna'  => 'fas fa-users',
+    'pengaturan' => 'fas fa-cog'
+];
---
+++ config/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'dashboard' => 'default/index',
+    'master' => [
+        'kategori' => 'categories/index',
+        'lokasi' => 'locations/index'
+    ],
+    'pengguna'  => [
+        'semua pengguna' => 'users/index',
+        'roles' => 'roles/index'
+    ],
+    'pengaturan' => 'application/index'
+];
