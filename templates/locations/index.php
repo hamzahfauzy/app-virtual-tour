66d39abdff6271c4cd1836208a92687eadc15a30
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-12-17 17:47:56 +0700

    update button label

--- templates/locations/index.php
+++ templates/locations/index.php
@@ -8,7 +8,7 @@
                         <h5 class="text-white op-7 mb-2">Memanajemen data lokasi</h5>
                     </div>
                     <div class="ml-md-auto py-2 py-md-0">
-                        <a href="index.php?r=locations/create" class="btn btn-secondary btn-round">Buat Pengguna</a>
+                        <a href="index.php?r=locations/create" class="btn btn-secondary btn-round">Buat Lokasi</a>
                     </div>
                 </div>
             </div>
